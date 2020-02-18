94057d6b3f12c6c921c3331739a987a7c2b1f974
JacobDeepu <JacobDeepu@users.noreply.github.com>
2020-02-18 14:03:06 +0000

    NB1: Build missing libwifi-hal-ctrl
    Change-Id: I84b7a568183012680cedf86da2a1b145df6c963d

--- device.mk
+++ device.mk
@@ -160,6 +160,9 @@ PRODUCT_PACKAGES += \
     libhwbinder.vendor
 
 # GPS
+PRODUCT_PACKAGES += \
+    libwifi-hal-ctrl
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
     $(LOCAL_PATH)/configs/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
