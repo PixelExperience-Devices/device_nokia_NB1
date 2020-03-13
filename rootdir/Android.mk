bf5cfeb230cc65f267e70728fcc582e95e2e0d5f
Jack Pham <jackp@codeaurora.org>
2020-03-12 23:59:56 -0700

    NB1: init.qcom.usb.rc: Consolidate init.msm.usb.configfs.rc
    Remove obsolete android_usb based property triggers now
    that configfs is always used. Merge in init.msm.configfs.rc
    so we now have just a single .rc init file.
    
    Also remove all other references to /sys/class/android_usb/*.
    
    Change-Id: I06eba8e715ccee5974dc9de8ac19df6c79ae0c45

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -73,14 +73,6 @@ LOCAL_VENDOR_MODULE    := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.msm.usb.configfs.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.msm.usb.configfs.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.nb1.camera.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
