6dd2fcf2e11c383fc235972dc538a361a6d3bc55
Andrea <linckandrea01@gmail.com>
2021-02-20 12:47:06 +0100

    NB1: Disable sched_enable_hmp
     * This should be disabled since NB1 is using EAS
    
    Change-Id: I437cfa2f5f018b457e5054f9c8124e6101d6fc06

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,7 +76,7 @@ BOARD_HAVE_QCOM_FM := true
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
-BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 sched_enable_power_aware=1
+BOARD_KERNEL_CMDLINE += sched_enable_hmp=0 sched_enable_power_aware=1
 BOARD_KERNEL_CMDLINE += service_locator.enable=1 swiotlb=2048 androidboot.configfs=true
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a800000.dwc3 loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
