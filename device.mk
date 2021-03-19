5c916fb360bb7de7c7bc854c84e27c20273a6397
Alex Damaratski <alexeydomoratsky1@gmail.com>
2021-03-19 09:29:19 +0300

    NB1: Build Mock Powerstats HAL
    * E BatteryStatsService: Unable to load Power Hal or power.stats HAL
    
    Signed-off-by: Alex Damaratski <alexeydomoratsky1@gmail.com>
    Change-Id: Id9abdc668108e3a864056778acca1d2889316fe0

--- device.mk
+++ device.mk
@@ -316,7 +316,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.msm8998-libperfmgr
+    android.hardware.power-service.msm8998-libperfmgr \
+    android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
