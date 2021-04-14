b648462be1ea8a42b8b8cbe9bbcfa3dec5538301
Chirayu Desai <chirayudesai1@gmail.com>
2021-04-15 02:09:34 +0530

    NB1: rro_overlays: Move CarrierConfig to RRO
    Change-Id: Ia57acd244608848f70fdcc30d5694c919db60671

--- device.mk
+++ device.mk
@@ -351,6 +351,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.1 \
     android.hardware.radio.config@1.0 \
     android.hardware.radio.deprecated@1.0 \
+    CarrierConfigOverlay \
     rild \
     libxml2 \
     librmnetctl
