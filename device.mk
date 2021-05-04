4bc132d7986a4723582d6dd69502c2ee51950b85
enesykaya <enesykaya@gmail.com>
2021-05-04 04:29:01 +0300

    NB1: powerhint: Import powerhint.json
    * Also add schedtune boost

--- device.mk
+++ device.mk
@@ -319,7 +319,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.msm8998-libperfmgr
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Protobuf
 PRODUCT_PACKAGES += \
