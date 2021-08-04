7a48adac8ec4f29acfbbe32be8e455e31f0ab904
George <geoster3d@gmail.com>
2021-08-04 21:46:55 +0300

    NB1: Build 4.0 audio HAL to match stock

--- device.mk
+++ device.mk
@@ -23,8 +23,10 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
+    android.hardware.audio@4.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-service \
+    android.hardware.audio.effect@4.0-impl \
     android.hardware.soundtrigger@2.2-impl
 
 PRODUCT_PACKAGES += \
