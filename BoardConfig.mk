8a89de84082fd339cd9fd5b6af75732e600e44da
Ethan Chen <intervigil@gmail.com>
2021-08-04 21:31:15 +0300

    NB1: Move system properties to system.prop

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,6 +86,7 @@ TARGET_BOARD_PLATFORM := msm8998
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno540
 
 # Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # QCOM hardware
