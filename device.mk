42a404f35d02ed002aaecaf8bd9b5795ce03982b
George <geoster3d@gmail.com>
2021-08-06 22:56:19 +0300

    NB1: Cleanup DRM stack
    * Drop source built components
    * Move firmware to vendor

--- device.mk
+++ device.mk
@@ -121,7 +121,8 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.1-service.clearkey
 
 # Exclude sensor from InputManager
 PRODUCT_COPY_FILES += \
