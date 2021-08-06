2a174e105ff1136988fa5ddea26f8c6c26f126bc
George <geoster3d@gmail.com>
2021-08-06 20:23:46 +0300

    NB1: Drop audio effect service
    * It doesn't exist

--- device.mk
+++ device.mk
@@ -18,7 +18,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl \
     android.hardware.audio.service \
-    android.hardware.audio.effect@2.0-service \
     android.hardware.audio.effect@4.0-impl \
     android.hardware.soundtrigger@2.2-impl
 
