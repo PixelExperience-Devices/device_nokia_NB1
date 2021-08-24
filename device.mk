1fc6872548a1780576184d92cf82360d600c373d
George <geoster3d@gmail.com>
2021-08-24 07:53:41 +0300

    Revert "NB1: Adding Audio HAL V5: Direction API"
    * Build pie audio HAL to match stock HAL.
    
    This reverts commit 3e760f4a59134020633285b46da8bf94dbfc9786.

--- device.mk
+++ device.mk
@@ -16,9 +16,9 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@5.0-impl \
+    android.hardware.audio@4.0-impl \
     android.hardware.audio.service \
-    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio.effect@4.0-impl \
     android.hardware.soundtrigger@2.2-impl
 
 PRODUCT_PACKAGES += \
