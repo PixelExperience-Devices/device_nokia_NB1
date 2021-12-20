5a6efff0435fc6910efe9213c56e936dcd3fe547
emufan4568 <geoster3d@gmail.com>
2021-12-20 15:00:08 +0200

    NB1: Remove unused ant xml

--- device.mk
+++ device.mk
@@ -73,9 +73,6 @@ PRODUCT_PACKAGES += \
     com.dsi.ant.antradio_library \
     com.dsi.ant@1.0.vendor
 
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.a2dp@1.0.vendor \
