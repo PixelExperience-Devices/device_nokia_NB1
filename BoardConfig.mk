fbb346f1313f7e8a8e8fb4adcf6ed12d31d7f1a4
LuK1337 <priv.luk@gmail.com>
2021-12-23 16:52:20 +0200

    NB1: sepolicy: Label vendor.camera.aux.packagelist
    `E libc    : Access denied finding property "vendor.camera.aux.packagelist"`
    
    Change-Id: I5bfbab138d752977edb337977d8272068f4ed792

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -140,6 +140,7 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
 # Sepolicy
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Treble
