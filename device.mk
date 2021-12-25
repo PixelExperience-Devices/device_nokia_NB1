5043a00fc61b383ebb343246ad3d0d31fdd1750b
c79 <vexed@riseup.net>
2021-12-25 19:00:30 +0200

    NB1: Fix bluetooth audio

--- device.mk
+++ device.mk
@@ -81,8 +81,13 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
     Bluetooth \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor:32 \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor:32 \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor:32 \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor:32 \
     vendor.qti.hardware.btconfigstore@2.0.vendor \
     libldacBT_bco
 
