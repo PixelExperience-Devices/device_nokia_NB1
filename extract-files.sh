1d294652d1a5d373f7be1863ab416b4b24b03d14
c79 <vexed@riseup.net>
2022-01-10 16:47:19 +0200

    NB1: Fix libmmcamera2_stats_modules.so

--- extract-files.sh
+++ extract-files.sh
@@ -49,7 +49,7 @@ function blob_fixup() {
         ## NB1 Patches
         # Patch gx_fpd for VNDK support
         vendor/bin/gx_fpd)
-            "${PATCHELF}" --remove-needed "libunwind.so" "${2}" 
+            "${PATCHELF}" --remove-needed "libunwind.so" "${2}"
             "${PATCHELF}" --remove-needed "libbacktrace.so" "${2}"
             "${PATCHELF}" --add-needed "liblog.so" "${2}"
             "${PATCHELF}" --add-needed "libshim_binder.so" "${2}"
@@ -58,6 +58,13 @@ function blob_fixup() {
         vendor/lib64/hw/gxfingerprint.default.so)
             sed -i -e 's|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g' "${2}"
             ;;
+
+        # Load sensors.rangefinder.so from /vendor partition
+        vendor/lib/libmmcamera2_stats_modules.so)
+            sed -i -e 's|system/lib64/sensors.rangefinder.so|vendor/lib64/sensors.rangefinder.so|g' "${2}"
+            sed -i -e 's|system/lib/sensors.rangefinder.so|vendor/lib/sensors.rangefinder.so|g' "${2}"
+            ;;
+
         # Use vendor version of libgui
         vendor/lib/hw/camera.msm8998.so)
             "${PATCHELF}" --replace-needed "libgui.so" "libgui_vendor.so" "${2}"
