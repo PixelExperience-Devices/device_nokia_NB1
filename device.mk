58465de2344e43937b260a87d40e719f7c3a9766
GPUCode <geoster3d@gmail.com>
2022-05-24 16:00:01 +0300

    NB1: Fix missing seperator

--- device.mk
+++ device.mk
@@ -400,7 +400,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power.stats@1.0-service.mock \
      android.hardware.power-service.nokia_msm8998-libperfmgr
 
 PRODUCT_COPY_FILES += \
