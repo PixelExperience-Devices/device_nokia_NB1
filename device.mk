be857c5385682c548aaeef7d560822eef57e404f
GPUCode <geoster3d@gmail.com>
2022-05-26 16:36:34 +0300

    NB1: Build libtinyxml

--- device.mk
+++ device.mk
@@ -529,6 +529,10 @@ USE_DEX2OAT_DEBUG := false
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 
+# TinyXML
+PRODUCT_PACKAGES += \
+    libtinyxml
+
 # Telephony
 PRODUCT_PACKAGES += \
     qti-telephony-hidl-wrapper \
