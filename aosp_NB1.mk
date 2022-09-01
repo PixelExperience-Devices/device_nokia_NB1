841ee3ad207c73cc795334cdbe9ad6a3a0332bb5
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-02 06:59:52 +0700

    NB1: Drop product_launched_with_*.mk
    * It seems Google stopped making those
    * Directly set PRODUCT_SHIPPING_API_LEVEL

--- aosp_NB1.mk
+++ aosp_NB1.mk
@@ -7,7 +7,9 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Indicate the first api level the device has been commercially launched on
+PRODUCT_SHIPPING_API_LEVEL := 27
 
 # Inherit some common PixelExperience stuff
 TARGET_INCLUDE_WIFI_EXT := true
