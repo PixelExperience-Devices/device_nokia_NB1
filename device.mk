00f089badc35f43b0998c02e7b34d3e3cb4a921a
log1cs <tuan73176@gmail.com>
2022-10-07 09:29:48 +0700

    NB1: Build android.hidl.allocator@1.0
    10-06 22:18:19.917  7696  7696 E vndksupport: Could not load /vendor/lib64/hw/vendor.qti.hardware.qseecom@1.0-impl.so from default namespace: dlopen failed: library "android.hidl.allocator@1.0.so" not found: needed by /vendor/lib64/hw/vendor.qti.hardware.qseecom@1.0-impl.so in namespace (default).

--- device.mk
+++ device.mk
@@ -211,7 +211,9 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0.vendor \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0.vendor
+    android.hidl.manager@1.0.vendor \
+    android.hidl.allocator@1.0 \
+    android.hidl.allocator@1.0.vendor
 
 PRODUCT_PACKAGES += \
     libhidltransport \
