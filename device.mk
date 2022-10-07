4b25a9366b9073da79c9717e5cd0267eac816670
Marco Marinho <marcomarinho12@gmail.com>
2022-10-07 20:16:58 +0700

    NB1: Build display packages

--- device.mk
+++ device.mk
@@ -152,6 +152,8 @@ PRODUCT_PACKAGES += \
     libqdMetaData \
     libqdMetaData.system \
     libqdMetaData.vendor
+    libsdmcore \
+    libsdmutils
 
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
